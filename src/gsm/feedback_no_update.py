c8a75a2e945598b707ce4a2b6f14e51235bf82ee
Aman Madaan <amadaan@cs.cmu.edu>
2023-07-16 18:36:22 -0400

    #9 Sync GSM codebase from anon repo

--- src/gsm/feedback_no_update.py
+++ src/gsm/feedback_no_update.py
@@ -5,18 +5,17 @@ from src.utils import Prompt
 
 
 class GSMFeedback(Prompt):
-    def __init__(self, engine: str, prompt_examples: str, temperature: float, max_tokens: int = 600) -> None:
+    def __init__(self, engine: str, prompt_examples: str, temperature: float, max_tokens: int = 300) -> None:
         super().__init__(
             question_prefix="",
             answer_prefix="",
             intra_example_sep="\n\n",
-            inter_example_sep="\n\n### END ###\n\n",
+            inter_example_sep="\n\n### END ###n\n",
             engine = engine,
             temperature = temperature
         )
-        
         self.max_tokens = max_tokens
-        self.instruction = "# There is an error in the code above because of lack of understanding of the question. What is the error? To find the error, go through semantically complete blocks of the code, and check if everything looks good." if "naive" not in prompt_examples else "# There is an error in the code above."
+        self.instruction = "# There is an error in the code above because of lack of understanding of the question. What is the error? To find the error, go through semantically complete blocks of the code, and check if everything looks good."
         self.setup_prompt_from_examples_file(prompt_examples)
 
     def setup_prompt_from_examples_file(self, examples_path: str) -> str:
@@ -25,8 +24,6 @@ class GSMFeedback(Prompt):
     
     def __call__(self, solution: str):
         generation_query = self.make_query(solution=solution)
-        print(generation_query)
-        # print(1/0)
         output = openai_api.OpenaiAPIWrapper.call(
             prompt=generation_query,
             engine=self.engine,
@@ -35,41 +32,23 @@ class GSMFeedback(Prompt):
             temperature=self.temperature,
         )
         
-        
         entire_output = openai_api.OpenaiAPIWrapper.get_first_response(output)
-        print(entire_output)
         if "### END" in entire_output:
             entire_output = entire_output.split("### END")[0]
-
-        improved_soln = entire_output.split("def solution():")[1]
+        solution = entire_output.split("def solution():")[1]
         feedback = entire_output.split("def solution():")[0]
-        improved_soln = "def solution():" + improved_soln.rstrip()
-        self.update_prompt(solution=solution, improved_soln=improved_soln, feedback=feedback)
-        return {"solution": improved_soln, "feedback": feedback}
+        solution = "def solution():" + solution.rstrip()
+        return {"solution": solution, "feedback": feedback}
 
     def make_query(self, solution: str):
-        
         solution = f"""{self.question_prefix}{solution}{self.intra_example_sep}{self.instruction}{self.answer_prefix}"""
         return f"{self.prompt}{solution}"
-    
-    
-    def update_prompt(self, solution: str, improved_soln: str, feedback: str):
-        prefix = f"""{self.question_prefix}{solution}{self.intra_example_sep}{self.instruction}{self.answer_prefix}"""
-        
-        gen_ans = f"""
-
-{feedback}
-
-{improved_soln.rstrip()}{self.inter_example_sep}"""
-
-        new_example = f"{prefix}{gen_ans}"
-        self.prompt = f"{self.prompt}{new_example}"
     
 
 def test():
     task_fb = GSMFeedback(
-        prompt_examples="data/prompt/gsm/pal/feedback.txt",
-        engine="gpt-3.5-turbo",
+        prompt_examples="data/prompt/gsm/feedback.txt",
+        engine="code-davinci-002",
         temperature=0.7,
     )
 
@@ -84,8 +63,6 @@ def test():
     feedback_and_solution = task_fb(wrong_soln)
     print(feedback_and_solution["feedback"])
     print(feedback_and_solution["solution"])
-    
-    print(task_fb.prompt)
     
 
 if __name__ == '__main__':
